b5d385244e02ca07e7724b7415862744a24cdfb7
Pablo Cubillos <pcubillos326@gmail.com>
2019-10-30 16:25:33 -0500

    Añadiendo graphql con express, creando schema y la conexion con el resolver

--- graphql-server/index.js
+++ graphql-server/index.js
@@ -1,9 +1,34 @@
 import express from 'express';
+//graphql
+import graphqlHTTP from 'express-graphql';
+import schema from './schema'; 
 
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('Todo ok');
 });
+
+//Resolver
+const root = {
+    cliente: () => {
+        return {
+            "id": 12781782,
+            "nombre": "Pablo",
+            "apellido": "Cubillos",
+            "empresa": "RC",
+            "email": "pablo@correo.com"
+        }
+    }
+};
+
+app.use('/graphql', graphqlHTTP({
+    //Que schema va a usar en esa url
+    schema,
+    //resolver se pasa como rootvalue
+    rootValue: root,
+    //utilizar graphiql
+    graphiql: true
+}));
 
 app.listen(8000, () => console.log('Server funcionando'));
